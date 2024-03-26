a7335b06d0def7fb3b3fc0734be2a105b2f19582
KH <h.kikuchi.sys23@morijyobi.ac.jp>
2024-03-26 10:42:05 +0900

    exerciseです

---
+++ practice_6/exercise.js
@@ -0,0 +1,20 @@
+function text(){
+    let wordValue = document.getElementById("wordInput").value;
+    let result = document.getElementById("result");
+    result.textContent = wordValue;
+}
+function btnClick(){
+    result.className = "no";
+}
+function btnClick1(){
+    result.className = "red";
+}
+function btnClick2(){
+    result.className = "green";
+}
+function btnClick3(){
+    result.className = "blue";
+}
+function btnClick4(){
+    result.className = "yellow";
+}
